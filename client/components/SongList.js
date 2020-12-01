f7f8b6ffdbc42490e7f1a7e5fe92e30646fedc70
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-12-01 12:02:09 -0600

    SongCreate component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import { divide } from 'lodash';
+
 
 class SongList extends Component {
   renderSongs() {
@@ -17,9 +20,17 @@ class SongList extends Component {
     if (this.props.data.loading) { return <div>Loading... </div>;} 
 
     return (
-      <ul className="collection">
-        {this.renderSongs()}
-      </ul>
+      <div>
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+        <Link
+          to="/songs/new"
+          className="btn-floating btn-large red right"
+        >
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
     );
   }
 }
--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import App from './components/App';
+import SongCreate from './components/SongCreate'
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
@@ -14,6 +15,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
         </Route>
       </Router>
     </ApolloProvider>
