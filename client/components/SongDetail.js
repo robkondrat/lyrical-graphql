ce01f7973bceeebb491cce72a49d612a367d726c
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-12-02 13:02:53 -0600

    can create lyrics, made LyricList

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
@@ -12,6 +14,8 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList />
+        <LyricCreate songId={this.props.params.id} />
       </div>
     )
   }
