3f461e8b80b449b43e6854ca559197da1c032caf
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-12-02 12:34:49 -0600

    can delete songs from SongList, created SongDetail component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
-import { divide } from 'lodash';
+import query from '../queries/fetchSongs';
 
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
+  }
+  
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} classname="collection-item">
-          {song.title}
+        <li 
+          key={id} 
+          className="collection-item"
+          // onClick={() => }
+        >
+          {title}
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(id)}
+          >
+            delete
+          </i>
         </li>
       )
     })
@@ -35,13 +50,14 @@ class SongList extends Component {
   }
 }
 
-const query = gql`
-  {
-    songs {
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
       id
-      title
     }
   }
 `;
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
